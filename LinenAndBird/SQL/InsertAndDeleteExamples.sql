188f796e96703a20143542836ae39dd14472757c
Matthew Gonzales <gonzalesmattg@gmail.com>
2021-09-14 21:41:26 -0500

    - did inserts/updates, deletes, api completion, MapReader and more

---
+++ LinenAndBird/SQL/InsertAndDeleteExamples.sql
@@ -0,0 +1,16 @@
+insert into Birds(Type,Color,Size,Name)
+values(0,'yellow','small','darcy')
+
+update Birds
+Set Color = 'blueberry',
+	Size = 'small',
+	--Name = Name + ' - ' + cast(o.Price as varchar(10)) + 'SOLD'
+	Name = Name + ' - SOLD'
+output inserted.*, deleted.* -- because update does both inserting and deleting
+from birds b
+	/*join orders o 
+		on o.BirdId = b.Id*/
+--Where Color = 'yellow' -- if no where clause is used then all rows Color will be set
+
+select * from birds;
+select * from orders;
